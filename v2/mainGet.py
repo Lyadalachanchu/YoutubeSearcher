1c7013273c774371869c9a52bebeecd77481181e
Lyadalachanchu <56978360+Lyadalachanchu@users.noreply.github.com>
2020-06-04 22:58:45 +0200

    Update mainGet.py

--- v2/mainGet.py
+++ v2/mainGet.py
@@ -7,15 +7,18 @@ import os
 
 def mainGet(apikey, cID, path):
 	api_key = apikey
-	captions = []
 
 	try:
 		os.mkdir(path+'/captions/'+cID)
-		gacfc(cID, path)
-		captionSaver(cID, path)
 	except:
 		print("file exists")
 
+
+	captions = []
+
+
+	#gacfc(cID, path)
+	#captionSaver(cID, path)
 
 	for filename in os.listdir(path+'/captions/'+cID):
 		try:
@@ -23,13 +26,37 @@ def mainGet(apikey, cID, path):
 				mynewlist = pickle.load(f)
 				#print(mynewlist)
 				captions.append(mynewlist)
+				#print(mynewlist)
 				
 		except Exception as e:
 			#print(e)
 			print("No transcipts available")
 
+	#Searching Part
+	wsdv = []
+	for x, caption in enumerate(captions):
+		counter = 0
+		for z, line in enumerate(caption):
+			for a, word in enumerate(line['text'].split()):
+				wsdv.append([word, counter])
+				counter += 1
+		
 
-	return(captions)
+	query = "or ski"
+	query = query.split()
+	store = []
+	location = []
+	for i in range(len(wsdv)):
+		if (wsdv[i][0] == query[0]):
+			for n in range(len(query)):
+				if(query[n] == wsdv[i+n][0]):
+					pass
+				else:
+					store = []
+					break
+				if(n == len(query)-1):
+					print('Found query at {}'.format(i))
+					store.append(i)
+					location = [i, i+len(query)-1]
 
-
-
+	print(location)
