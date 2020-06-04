fbf1aac827254b200a40003158c552e0d288d5f4
MxDkl <max@dekelnet.com>
2020-06-04 13:31:50 +0200

    Update main.py

--- v2/main.py
+++ v2/main.py
@@ -1,5 +1,24 @@
 from mainGet import mainGet
+import pickle
 
 
-captions = mainGet(apikey='AIzaSyAZjUllFM931vhUM7JdOAl_eysGcTzGZtk', cID='UCr3cBLTYmIK9kY0F_OdFWFQ', path='/Users/maxdekel/Desktop/stuff/YoutubeSearcher')
-print(captions)
+
+query = "well worth"
+
+
+path = '/Users/maxdekel/Desktop/stuff/YoutubeSearcher'
+key = 'AIzaSyAZjUllFM931vhUM7JdOAl_eysGcTzGZtk'
+cID = 'UCr3cBLTYmIK9kY0F_OdFWFQ'
+
+captions = mainGet(apikey=key, cID=cID, path=path)
+#print(captions)
+
+
+for x in captions:
+	for a in x[0:len(x)-2]:
+		try:
+			txt = a['text']
+			if query in txt:
+				print(a['start'], x[len(x)-1])
+		except:
+			print("something went wrong")
